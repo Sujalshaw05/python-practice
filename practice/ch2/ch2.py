a45c23c1b01c81b7ce6fb99b47f2c990607a7563
unknown <sujalshaw204@gmail.com>
2025-09-30 17:25:49 +0530

    chapter 2 completed

---
+++ practice/ch2/ch2.py
@@ -0,0 +1,14 @@
+a=1  # int type
+  
+b=2.3 # float type
+c=None # none type
+d=False #false type
+e="sujal" # string type
+print(a+b)
+
+print(type(a)) # getting the type of a variable
+
+f=int(b) # type conversion
+print(f)
+
+p= int(input("enter a number")) # input function takes  input as a string we need to convert that into int to perform arithmetic opration
