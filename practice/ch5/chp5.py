254ef979184780958ad04a7306756ec0a80de98b
Sujalshaw05 <sujalshaw204@gmail.com>
2025-10-06 19:30:59 +0530

    Update chp5.py

--- practice/ch5/chp5.py
+++ practice/ch5/chp5.py
@@ -1,38 +1,38 @@
 
-# words={
-#     "madad":"help",
-#     "kurshi":"chair",
-#     "billi":"cat"
-# }
+words={
+    "madad":"help",
+    "kurshi":"chair",
+    "billi":"cat"
+}
 
-# word=input("enter the word")
+word=input("enter the word")
 
-# print(words[word])
+print(words[word])
 
-# s=set()
-# s.add(20)# 
-# s.add(20.0)
-# s.add("20")
-# print(len(s))# because python first comares the value then the data type in python 20 == 20.0 so length is 2
-
-
+s=set()
+s.add(20)# 
+s.add(20.0)
+s.add("20")
+print(len(s))# because python first comares the value then the data type in python 20 == 20.0 so length is 2
 
 
-# d={}
-# name= input("enter the name")
-# lang=input("enter the language")
-# d.update({name:lang})
-# name= input("enter the name")
-# lang=input("enter the language")
-# d.update({name:lang})
-# name= input("enter the name")
-# lang=input("enter the language")
-# d.update({name:lang})
-# name= input("enter the name")
-# lang=input("enter the language")
-# d.update({name:lang})
 
-# print(d)
+
+d={}
+name= input("enter the name")
+lang=input("enter the language")
+d.update({name:lang})
+name= input("enter the name")
+lang=input("enter the language")
+d.update({name:lang})
+name= input("enter the name")
+lang=input("enter the language")
+d.update({name:lang})
+name= input("enter the name")
+lang=input("enter the language")
+d.update({name:lang})
+
+print(d)
 
 
 
